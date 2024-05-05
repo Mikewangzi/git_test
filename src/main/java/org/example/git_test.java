f97ae474629f935e16942e17591e27f75ac1ef65
王子俊 <Wzj18395512425@outlook.com>
2024-05-05 14:17:17 +0800

    temp version1

--- src/main/java/org/example/git_test.java
+++ src/main/java/org/example/git_test.java
@@ -9,5 +9,6 @@ public class git_test {
         System.out.println("hello,git!!");
         System.out.println("hello,git!!");
         System.out.println("hello,git!!");
+        System.out.println("hello,git!!");
     }
 }
