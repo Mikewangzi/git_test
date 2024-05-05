74125a65038fbf5c9eff4eed1230c63ef8d327c7
王子俊 <Wzj18395512425@outlook.com>
2024-05-05 14:08:25 +0800

    version1

---
+++ src/main/java/org/example/git_test.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @Author: 王子俊
+ * @since: 2024/05/05
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello,git!!");
+    }
+}
