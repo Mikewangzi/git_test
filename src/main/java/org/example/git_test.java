33572faa2235951a89784227bb5da99092b4a6a5
王子俊 <Wzj18395512425@outlook.com>
2024-05-05 14:11:48 +0800

    version2

--- src/main/java/org/example/git_test.java
+++ src/main/java/org/example/git_test.java
@@ -7,5 +7,7 @@ package org.example;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello,git!!");
+        System.out.println("hello,git!!");
+        System.out.println("hello,git!!");
     }
 }
